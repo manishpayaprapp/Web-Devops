57e6a0c205d34b8236f8bd33339e05d86e2f3ff2
Manish <manish.payaprapp.01@gmail.com>
2025-12-15 20:29:10 +0530

    Update calculator.js

--- week-3/Methods/calculator.js
+++ week-3/Methods/calculator.js
@@ -17,16 +17,16 @@ app.get('/divide',function(req,res){
     })
 })
 app.get('/sum',function(req,res){
-    let a = req.query.a;
-    let b =req.query.b;
+    let a =parseInt(req.query.a);
+    let b =parseInt(req.query.b);
     let ans =a+b
     res.json({
         ans:ans
     })
 })
 app.get('/subtraction',function(req,res){
-    let a = req.query.a;
-    let b =req.query.b;
+    let a = parseInt(req.query.a);
+    let b =parseInt(req.query.b);
     let ans =a-b
     res.json({
         ans:ans
