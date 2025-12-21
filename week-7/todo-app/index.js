76d32b484016343816fb7cf2a34de6510885813f
Manish <manish.payaprapp.01@gmail.com>
2025-12-21 20:17:48 +0530

    Create index.js

---
+++ week-7/todo-app/index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const JWT_SECRECT = "Key123";
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://manishpayaprapp02_db_user:50DoElmPElDeFCHc@cluster0.sz9hwut.mongodb.net/todo-Manish"
+);
+
+const { UserModel, TodoModel } = require("./db.js");
+const {auth}= require("./auth.js");
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  await UserModel.create({
+    email: email,
+    password: password,
+    name: name
+  });
+
+  res.json({
+    msg: "you are signed up"
+  });
+});
+
+app.post("/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await UserModel.findOne({
+    email: email,
+    password: password
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      { id: user._id.toString() },
+      JWT_SECRECT
+    );
+
+    res.json({ token });
+  } else {
+    res.status(403).json({
+      msg: "Wrong Credentials"
+    });
+  }
+});
+
+app.post("/todo", auth, async (req, res) => {
+  const userid = req.Userid;
+  const title = req.body.title;
+  const done = req.body.done;
+
+  await TodoModel.create({
+    userId: userid, 
+    title: title,
+    done: done
+  });
+
+  res.json({
+    msg: "Todo is added"
+  });
+});
+
+app.get("/todos", auth, async (req, res) => {
+  const userid = req.Userid;
+
+  const todos = await TodoModel.find({
+    userId: userid 
+  });
+
+  res.json({
+    todos: todos
+  });
+});
+app.listen(3000);
