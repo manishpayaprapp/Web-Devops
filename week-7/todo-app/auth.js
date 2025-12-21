fba5edd2d8fee3c2886ad7abcc1273093bcb92c6
Manish <manish.payaprapp.01@gmail.com>
2025-12-21 20:18:29 +0530

    Create auth.js

---
+++ week-7/todo-app/auth.js
@@ -0,0 +1,13 @@
+function auth(req, res, next) {
+  const token = req.headers.token;
+  const decodedData = jwt.verify(token, JWT_SECRECT);
+
+  if (decodedData) {
+    req.Userid = decodedData.id;
+    next();
+  } else {
+    res.status(403).json({
+      msg: "Wrong cridentials"
+    });
+  }
+}
