2c98268a65930369c00a8d75023c1b75c16db8bd
Manish <manish.payaprapp.01@gmail.com>
2026-01-27 10:53:13 +0530

    Update user.js

--- week-8/course-selling-app/routes/user.js
+++ week-8/course-selling-app/routes/user.js
@@ -1,8 +1,35 @@
 const {Router}=require("express");
 const userRouter =Router();
-
-userRouter.post("/signup",(req,res)=>{
-
+const {z}= require("zod");
+const {userModel}=require("../db");
+userRouter.post("/signup",async(req,res)=>{
+const{email,password,firstName,lastName}=req.body;
+const requireBody =z.object({
+    email:z.string().min(3).max(50),
+    password:z.string().min(3).max(50),
+    firstName:z.string().min(3).max(50),
+    lastName:z.string().min(3).max(50)
+})
+const parsedDataWithSuccess =requireBody.safeParse(req.body);
+if(!parsedDataWithSuccess.success){
+    res.json({
+      msg:"Incorrect Format",
+      error:parsedDataWithSuccess.error
+    })
+    return
+  }
+  const hashedPassword =await bcrypt.hash(password,5);
+  try{await userModel.create({
+    email,
+    password: hashedPassword,
+    firstName,
+    lastName
+})}
+catch(e){
+    res.status(403).json({
+        msg:"Wrong Cridentials"
+    })
+}
 });
 userRouter.post("/login",(req,res)=>{
 
