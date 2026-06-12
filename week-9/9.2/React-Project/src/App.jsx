fbe30022b4a7060bf5b4a6d42df4f88395ba7aa7
Manish s p <manish.payaprapp.01@gmail.com>
2026-06-12 20:53:32 +0530

    learned useEffect,mount,unmount,cleanup

--- week-9/9.2/React-Project/src/App.jsx
+++ week-9/9.2/React-Project/src/App.jsx
@@ -31,33 +31,81 @@
 // export default App
 
 // ---------------------------------------------UseEffect---------------------------------
+// import { useState,useEffect } from "react";
+
+// function App(){
+//   const [CounterVisible,setCounterVisible]=useState(true);
+
+//   useEffect(function(){
+//     setInterval(function(){
+//       setCounterVisible(c => !c)
+//     },5000);  
+//   },[])
+//   return <div>
+//     <h1>UseEffect</h1>
+//     {CounterVisible && <Counter></Counter>} 
+//     {/* OR */}
+//     {/* {CounterVisible ? <Counter></Counter> : null} */}
+//   </div>
+// }
+
+// function Counter(){
+//   const [count,SetCount]=useState(0);
+//   console.log("Without UseEffect")
+//   useEffect(function(){
+//     setInterval(function(){ //mount
+//       // setCount(count=>count+1)  // OR
+//       SetCount(function(currentValue){
+//         return currentValue+1;
+//       });
+//     },1000)
+//     return function(){ //unmount
+//       clearInterval(clock)
+//     }
+//   },[]);
+
+//   return <div>
+//     <h1>
+//       {count}
+//     </h1>
+//   </div>
+// }
+
+// export default App
+
+
+//----------------------------------------------
 import { useState,useEffect } from "react";
-
 function App(){
-  return <div>
-    <h1>UseEffect</h1>
-    <Counter></Counter>
-  </div>
+  const [count,setCount]=useState(0);
+  const [count2,setCount2]=useState(0);
+  function increse(){
+    setCount(c=>c+1);
+  }
+  function decrese(){
+    setCount2(c=>c-1);
+  }
+return <div>
+  <Counter count={count} count2={count2}/>
+  <button id="count" onClick={increse}>Increse Count</button>
+  <button id="count" onClick={decrese}>Decrese Count</button>
+</div>
 }
-
-function Counter(){
-  const [count,SetCount]=useState(0);
-  console.log("Without UseEffect")
+function Counter(props){
   useEffect(function(){
-    setInterval(function(){
-      // setCount(count=>count+1)  // OR
-      SetCount(function(currentValue){
-        return currentValue+1;
-      });
-    },1000)
-    console.log("UseEffect");
-  },[]);
+    console.log("Mount")
+  },[])
+  
+  useEffect(function(){
+    console.log("count is changed")
 
+    return(function(){
+    console.log("Cleanup wala")
+    })
+  },[props.count,props.count2])
   return <div>
-    <h1>
-      {count}
-    </h1>
+    <h1>Counter1 {props.count}</h1><br></br>
+    <h1>Counter2 {props.count2}</h1>
   </div>
 }
-
 export default App
