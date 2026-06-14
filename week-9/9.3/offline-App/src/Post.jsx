987bc38f7a085b3ede56a6dbd8a59bbab86dc1a1
Manish s p <manish.payaprapp.01@gmail.com>
2026-06-14 20:50:59 +0530

    added a post component array

--- week-9/9.3/offline-App/src/Post.jsx
+++ week-9/9.3/offline-App/src/Post.jsx
@@ -1,34 +1,26 @@
-const style = {
-  width: 200,
-  backgroundColor: 'white',
-  borderRadius: 10,
-  borderColor: 'gray',
-  borderWidth: 1,
-  padding: 10,
-  paddingLeft: 30,
-  paddingRight: 50
-}
-
-export function PostComponent({ name, subtitle, image, time, discription }) {
+const style={width:200,backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1,padding:10,paddingLeft:30,paddingRight:50}
+export function PostComponent({ name, subtitle, image, time, discription }) { 
   return (
     <div style={style}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img src={image} style={{ width: 55, height: 55, borderRadius: '50%' }} />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={image}
+          style={{ width: 55, height: 55, borderRadius: "50%" }}
+        />
 
         <div style={{ marginLeft: 20 }}>
           <div style={{ fontSize: 15 }}>
             <b>{name}</b>
           </div>
           <div>{subtitle}</div>
-          {time !== undefined ? (
-            <div>
-              {time}. <span style={{ fontSize: 10 }}>🌐</span>
-            </div>
-          ) : null}
+          {(time!==undefined) ? <div>{time}. <span style={{fontSize:10}}>🌐</span></div>:null}
+          
         </div>
       </div>
-      <div style={{ marginTop: 10, fontSize: 16 }}>{discription}</div>
+      <div style={{ marginTop: 10, fontSize: 16 }}>
+        {discription}
+      </div>
     </div>
-  )
+  );
 }
 
