e65ef9d939186f70973e3d887ca646ad8e126c8e
Manish s p <manish.payaprapp.01@gmail.com>
2026-06-14 20:50:18 +0530

    added a post component array

--- week-9/9.3/offline-App/src/App.jsx
+++ week-9/9.3/offline-App/src/App.jsx
@@ -1,38 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import { PostComponent} from './Post'
 
 function App() {
+    const [posts,setPosts]=useState([]);
+
+  const PostComponents =posts.map(post => <PostComponent
+        name={post.name}
+        subtitle={post.subtitle}
+        image={post.image}
+        time={post.time}
+        discription={post.discription}
+  />)
+  function addPost(){
+    setPosts([...posts,{
+    name:"100Xdev",
+    subtitle:"23045 followers",
+    image:"https://projects.100xdevs.com/_next/image?url=https%3A%2F%2Fappx-wsb-gcp.akamai.net.in%2Fsubject%2F2023-01-17-0.17044360120951185.jpg&w=384&q=75",
+    time:"39 min ago",
+    discription:"Just completed my first React project today! 🚀 Learning something new every day."
+    }])
+  }
+
   return <div style={{background:"#C0C0C0",height:"100vh"}}>
-    <PostComponent/>
+    <div><button onClick={addPost}>ADD POST</button>
+      </div>
+    <div style={{display:"flex",justifyContent:"center"}}>
+      <div>
+        <div>{PostComponents}</div>
+      </div>
+    </div>
+    
   
     </div>
-}
-    
- 
-const style={width:200,backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1}
-
-function PostComponent(){
-  return (
-    <div style={style}>
-      <div style={{display:"flex"}}>
-        <img
-          src="https://projects.100xdevs.com/_next/image?url=https%3A%2F%2Fappx-wsb-gcp.akamai.net.in%2Fsubject%2F2023-01-17-0.17044360120951185.jpg&w=384&q=75"
-          style={{width:40,height:40,borderRadius:20}}
-        />
-
-        <div >
-          <div style={{fontSize:10,marginLeft:20}}>
-          <b>100Xdev</b>
-            <div>1,23,43,434 followers</div>
-            <div>12m</div>
-          </div>
-        </div>
-      </div>
-
-      <div style={{fontSize:12}}>
-        Discription
-      </div>
-    </div>
-  );
 }
 export default App
