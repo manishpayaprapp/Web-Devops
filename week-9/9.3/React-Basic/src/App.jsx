df79fb983e7a7ac0e4de10f59e696c44c9b40d5a
Manish s p <manish.payaprapp.01@gmail.com>
2026-06-15 17:20:44 +0530

    Learned some more basics of react

---
+++ week-9/9.3/React-Basic/src/App.jsx
@@ -0,0 +1,53 @@
+//------------------------------------------ Children -------------------------------------------
+
+// import './App.css'
+
+// function App() {
+//   return (
+//     <div style={{display:"flex"}}>
+//      <Card>
+//       <div>
+//         <input></input>
+//       </div>
+//      </Card>
+//      <Card>
+//       <div style={{cursor:"pointer"}}>
+//         Search
+//       </div>
+//      </Card>
+//     </div>
+    
+//   )
+// }
+
+// function Card({ children }) {
+//   return (
+//     <div style={{ backgroundColor: 'silver', borderRadius: 10 ,padding:10}}>
+//       {children}
+//     </div>
+//   )
+// }
+
+// export default App
+
+//----------------------------------------------------- List and  Key----------------------------------------
+
+// function App(){
+//   return (
+//     <div>
+//       {[
+//         <Todo key={2}title={"Go to gym"} done ={false}></Todo>,
+//         <Todo key={1}title={"Eat Food"} done ={true}></Todo>
+//       ]}
+//     </div>
+//   )
+// }
+
+// function Todo({title,done}){
+// return <div>
+//   {title} - {done ?"Done":"Do it"}
+// </div>
+// }
+
+// export default App
+
