5e413e334dc723adadc44ec30ad475912bd1adf9
Manish s p <manish.payaprapp.01@gmail.com>
2026-06-15 17:26:49 +0530

    error Boundary

--- week-9/9.3/React-Basic/src/App.jsx
+++ week-9/9.3/React-Basic/src/App.jsx
@@ -51,3 +51,94 @@
 
 // export default App
 
+// -------------------------------------------------------Error Boundary--------------------------------------
+
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: true,
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log("Error:", error);
+    console.log("Error Info:", errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            backgroundColor: "silver",
+            borderRadius: 10,
+            padding: 20,
+            margin: 20,
+          }}
+        >
+          <h3>Something went wrong</h3>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <ErrorBoundary>
+        <Card1 />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Card2 />
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+function Card1() {
+  // Uncomment to test
+  // throw new Error("Card1 crashed");
+
+  return (
+    <div
+      style={{
+        backgroundColor: "silver",
+        borderRadius: 10,
+        padding: 20,
+        margin: 20,
+      }}
+    >
+      Hi there
+    </div>
+  );
+}
+
+function Card2() {
+  return (
+    <div
+      style={{
+        backgroundColor: "silver",
+        borderRadius: 10,
+        padding: 20,
+        margin: 20,
+      }}
+    >
+      hello
+    </div>
+  );
+}
+
+export default App;
