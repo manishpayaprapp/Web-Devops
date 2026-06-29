71895100c5c2e910cc79d315503ab9ec5d116b36
Manish s p <manish.payaprapp.01@gmail.com>
2026-06-29 21:07:12 +0530

    Learned usePrev hook

--- week-11/customHooks/src/App.jsx
+++ week-11/customHooks/src/App.jsx
@@ -42,29 +42,51 @@
 
 // export default App
 
-// -------------------------------------
-import { useFetch} from "./hooks/useFetch"
-import { useState } from "react"
-function App(){
-const [currentPost, setCurrentPost] = useState(1);
-const {finalData, Loading} = useFetch("https://jsonplaceholder.typicode.com/todos/"+currentPost, 10)
-if(Loading){
-return (
-<>
-Loading.....
-</>)
-}
+// ------------------------------------- useFetch and use-re-fetch------------------------------------------------
+// import { useFetch} from "./hooks/useFetch"
+// import { useState } from "react"
+// function App(){
+// const [currentPost, setCurrentPost] = useState(1);
+// const {finalData, Loading} = useFetch("https://jsonplaceholder.typicode.com/todos/"+currentPost, 10)
+// if(Loading){
+// return (
+// <>
+// Loading.....
+// </>)
+// }
 
+// return (
+//   <>
+//   <button onClick={() =>{setCurrentPost(1)}}>1</button>
+//   <button onClick={() =>{setCurrentPost(2)}}>2</button>
+//   <button onClick={() =>{setCurrentPost(3)}}>3</button>
+//   <button onClick={() =>{setCurrentPost(4)}}>4</button>
+//   <button onClick={() =>{setCurrentPost(5)}}>5</button>
+//   {JSON.stringify(finalData)}
+//   </>
+// )
+// }
+
+// export default App
+
+import { useState } from "react";
+import { usePrev } from "./hooks/usePrev";
+function App(){
+  const [count, setCount] = useState(0);
+  const prev = usePrev(count)
 return (
   <>
-  <button onClick={() =>{setCurrentPost(1)}}>1</button>
-  <button onClick={() =>{setCurrentPost(2)}}>2</button>
-  <button onClick={() =>{setCurrentPost(3)}}>3</button>
-  <button onClick={() =>{setCurrentPost(4)}}>4</button>
-  <button onClick={() =>{setCurrentPost(5)}}>5</button>
-  {JSON.stringify(finalData)}
+  {count}
+  <br/> 
+  <button onClick={() =>{
+    setCount(count + 1)
+  }}>increase Count</button>
+  <button onClick={() =>{
+    setCount(count - 1)
+  }}>increase Count</button>
+  <br/>
+  previous vue of count is {prev}
   </>
 )
 }
-
 export default App
