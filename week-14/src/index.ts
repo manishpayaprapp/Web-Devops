6a91d4d411125912d7fc16d1e64ef602be22e868
Manish s p <manish.payaprapp.01@gmail.com>
2026-09-23 17:37:21 +0530

    Learned some more about TS

--- week-14/src/index.ts
+++ week-14/src/index.ts
@@ -47,24 +47,66 @@
 // }
 
 // ** Class Impliments the interface
-interface people{
-    name : string;
-    age : number;
-    // greet : () => string,
-}
+// interface people{
+//     name : string;
+//     age : number;
+//     isLegal : () => boolean;
+// }
 
-class Manager implements people{
-    name : string;
-    age : number;
+// class Manager implements people{
+//     // name : string;
+//     // age : number;
+//     // if ur not using any pre defined objects or variable from the interface then use the public in the constructor
+//     constructor(public name : string ,public  age : number ){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     isLegal(){
+//         return this.age > 18;
+//     }
+// }
 
-    constructor(name : string , age : number ){
-        this.name = name;
-        this.age = age;
-    }
-}
+// // extend of clss is different and implements is different
+// class God  extends Manager{
+//     constructor(name:string,age:number){
+//         super(name,age);
+//     }
+// }
 
-let user1 = new Manager("Manish", 21);
-console.log(user1.name);
+// let user1 = new Manager("Manish", 21);
+// console.log(user1.name);
+
+// -------------------------------------------
+// abstract classes  -- this samillar to interface but in this we can add a defualt function in abstaract class but we cannot do in the interface
+
+// abstract class User{
+//     name : string;
+//     constructor(name:string){
+//         this.name = name;
+//     }
+//     abstract greet () : string;
+//     hello (){
+//         console.log("Hi there");
+//     }
+// }
+
+// class Manager extends User{
+//     constructor(public name :string){
+//         super(name);
+//         this.name = name;
+//     }
+//     greet(){
+//         return "Hi" + this.name;
+//     }
+// }
+// console.log(Manager);
+
+// function nameOfUser(user:User):string{
+//     return(user.name)
+// }
+// console.log(nameOfUser)
+
+
 
 // * Types
 
@@ -95,3 +137,75 @@ console.log(user1.name);
 //     startDate : new Date(),
 //     department : "Development"
 // }
+
+
+// --- Assignment -----
+
+// type User = {
+//     name:string;
+//     age:number;
+// }
+// type Admin = {
+//     name:string;
+//     age:number;
+//     access:boolean
+// }
+
+// type functionInput = User | Admin;
+
+// let user1 ={
+//     name : "Manish",
+//     age : 21,
+//     access : true
+// }
+// function greet (user1:functionInput) : string{
+//     return "Welcome" + " " + user1.name
+// }
+
+// console.log(greet(user1))
+
+// interface User {
+//     firstName: string;
+//     lastName: string;
+//     age : number
+// }
+// let user1 = {
+//    firstName: "Manish",
+//    lastName: "SP",
+//     age :21
+// }
+// function isLegal(user1:User):boolean{
+//     return user1.age > 18
+// }
+// console.log(isLegal(user1))
+// ---- Arary --- 
+
+    // let nums = [1,2,3,4,5]
+    // function getMax(nums:number[]):number{
+    //     let max = 0;
+
+    //     for(let i = 0; i < nums.length;i++){
+    //         if(nums[i] > max)
+    //             max = nums[i]
+    //     }
+    //     return max
+
+    // }
+    // console.log(getMax(nums))
+
+    // ----------------
+
+    interface User {
+    firstName: string;
+    lastName: string;
+    age : number
+}
+let user1 = ([{
+   firstName: "Manish",
+   lastName: "SP",
+    age :21
+}])
+function isLegal(user1:User[]):User[]{
+    return user1.filter((user1) => user1.age > 18)
+}
+console.log(isLegal(user1))
